bf8ff0d1e8657583070666142bf572386cee6218
Sahil Mehra <sm.sahilmehra@gmail.com>
2022-04-03 15:56:30 +0100

    Added a custom equals method to Vertex.java

--- src/main/java/project/Vertex.java
+++ src/main/java/project/Vertex.java
@@ -4,23 +4,36 @@ import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class Vertex {
-  private String user;
-  private int hashCode;
+
+  private final String user;
+  private final int hashCode;
 
   public Vertex(String tweetName) {
     this.user = tweetName;
     this.hashCode = Objects.hashCode(tweetName);
   }
 
-    public String getName() {
-      return user;
-    }
-
-    @Override
-    public int hashCode(){
-      return this.hashCode;
-    }
+  public String getName() {
+    return user;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Vertex that = (Vertex) o;
+    return this.hashCode == that.hashCode;
+  }
+
+  @Override
+  public int hashCode() {
+    return this.hashCode;
+  }
+}
 
 
 
