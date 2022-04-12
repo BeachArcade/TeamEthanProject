2529e574ec86a6a3a0b68869afb9a97237640a6c
Sahil Mehra <sm.sahilmehra@gmail.com>
2022-04-12 19:25:16 +0100

    Added a check to insure an option was picked

--- src/main/java/project/GetGraph.java
+++ src/main/java/project/GetGraph.java
@@ -7,38 +7,39 @@ import java.sql.SQLOutput;
 
 public class GetGraph {
   public static void main(String[] args) throws IOException {
-    System.out.println("Missing arguments: <option> <output filepath> <input filepath>");
-    System.out.println("Choose an option");
-    System.out.println("1.) Create new graph");
-    System.out.println("2.) Create and invert new graph");
-    System.out.println("3.) Load Graph");
-    System.out.println("4.) Load and invert graph");
+    if (args.length == 0) {
+      System.out.println("Missing arguments: <option> <output filepath> <input filepath>");
+    } else {
+      System.out.println("Choose an option");
+      System.out.println("1.) Create new graph");
+      System.out.println("2.) Create and invert new graph");
+      System.out.println("3.) Load Graph");
+      System.out.println("4.) Load and invert graph");
 
-
-    if(args[0].equals("1") ){
+      if (args[0].equals("1")) {
         TwitterGraph twitterGraph = new TwitterGraph();
         Writer write = new Writer(new File(args[1]));
         write.writeToFile(twitterGraph);
-    } else if(args[0].equals("2")){
+      } else if (args[0].equals("2")) {
         TwitterGraph twitterGraph = new TwitterGraph();
         twitterGraph.invert();
         Writer write = new Writer(new File(args[1]));
         write.writeToFile(twitterGraph);
 
-    }else if(args[0].equals("3")){
+      } else if (args[0].equals("3")) {
         Writer writer = new Writer(new File(args[1]));
         Reader reader = new Reader(new File(args[2]));
         TwitterGraph twitterGraph = new TwitterGraph();
         reader.loadGraph(twitterGraph);
         writer.writeToFile(twitterGraph);
 
-    }else if(args[0].equals("4")){
+      } else if (args[0].equals("4")) {
         Writer writer = new Writer(new File(args[1]));
         Reader reader = new Reader(new File(args[2]));
         TwitterGraph twitterGraph = new TwitterGraph();
         reader.loadGraph(twitterGraph);
         writer.writeToFile(twitterGraph);
+      }
     }
   }
-
 }
