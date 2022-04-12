ae86fc7eb3a1afe6fe34712e07fdc0a373546488
Sahil Mehra <sm.sahilmehra@gmail.com>
2022-04-12 22:38:49 +0100

    Reverted to a previous version of the file

--- src/main/java/project/GetGraph.java
+++ src/main/java/project/GetGraph.java
@@ -1,45 +1,42 @@
 package project;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.SQLOutput;
 
 public class GetGraph {
+
   public static void main(String[] args) throws IOException {
-    if (args.length == 0) {
-      System.out.println("Missing arguments: <option> <output filepath> <input filepath>");
-    } else {
-      System.out.println("Choose an option");
-      System.out.println("1.) Create new graph");
-      System.out.println("2.) Create and invert new graph");
-      System.out.println("3.) Load Graph");
-      System.out.println("4.) Load and invert graph");
+    System.out.println("Missing arguments: <option> <output filepath> <input filepath>");
+    System.out.println("Choose an option");
+    System.out.println("1.) Create new graph");
+    System.out.println("2.) Create and invert new graph");
+    System.out.println("3.) Load Graph");
+    System.out.println("4.) Load and invert graph");
 
-      if (args[0].equals("1")) {
-        TwitterGraph twitterGraph = new TwitterGraph();
-        Writer write = new Writer(new File(args[1]));
-        write.writeToFile(twitterGraph);
-      } else if (args[0].equals("2")) {
-        TwitterGraph twitterGraph = new TwitterGraph();
-        twitterGraph.invert();
-        Writer write = new Writer(new File(args[1]));
-        write.writeToFile(twitterGraph);
+    if (args[0].equals("1")) {
+      TwitterGraph twitterGraph = new TwitterGraph();
+      Writer write = new Writer(new File(args[1]));
+      write.writeToFile(twitterGraph);
+      write.close();
+    } else if (args[0].equals("2")) {
+      TwitterGraph twitterGraph = new TwitterGraph();
+      twitterGraph.invert();
+      Writer write = new Writer(new File(args[1]));
+      write.writeToFile(twitterGraph);
 
-      } else if (args[0].equals("3")) {
-        Writer writer = new Writer(new File(args[1]));
-        Reader reader = new Reader(new File(args[2]));
-        TwitterGraph twitterGraph = new TwitterGraph();
-        reader.loadGraph(twitterGraph);
-        writer.writeToFile(twitterGraph);
+    } else if (args[0].equals("3")) {
+      Writer writer = new Writer(new File(args[1]));
+      Reader reader = new Reader(new File(args[2]));
+      TwitterGraph twitterGraph = new TwitterGraph();
+      reader.loadGraph(twitterGraph);
+      writer.writeToFile(twitterGraph);
 
-      } else if (args[0].equals("4")) {
-        Writer writer = new Writer(new File(args[1]));
-        Reader reader = new Reader(new File(args[2]));
-        TwitterGraph twitterGraph = new TwitterGraph();
-        reader.loadGraph(twitterGraph);
-        writer.writeToFile(twitterGraph);
-      }
+    } else if (args[0].equals("4")) {
+      Writer writer = new Writer(new File(args[1]));
+      Reader reader = new Reader(new File(args[2]));
+      TwitterGraph twitterGraph = new TwitterGraph();
+      reader.loadGraph(twitterGraph);
+      writer.writeToFile(twitterGraph);
     }
   }
 }
